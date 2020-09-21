51be0bd3cec3f8f974690ebaf554f2db6fabf528
unknown <ikmaldean92@gmail.com>
2020-09-21 10:55:53 +0700

    Initial Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layout/main')
+
+@section('title','Laravel')
+@section('container')
+
+    <div class="container">
+    <div class="row">
+    <div class="col-10">
+    hello world
+
+@endsection
